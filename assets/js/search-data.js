44ed1d39068dee8b7a1d8d12b5226aacc11edd3c
pelu10075 <116564879+pelu10075@users.noreply.github.com>
2025-06-11 04:27:14 +0000

    Deploying to gh-pages from @ pelu10075/pelu10075.github.io@9d6864cb8cb8432575909cb11702bb11831b02f8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
         },{id: "post-선형대수학이란",
         
           title: "선형대수학이란?",
